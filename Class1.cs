ed07ba8ba208d68c89cb99c80adb042a41c026ba
Vincenzo-Vicchiariello <v.vicchiariello@outlook.it>
2023-04-18 17:37:01 +0200

    Aggiungere i file di progetto.

---
+++ Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    public class Prodotto
+    {
+
+        private int codiceProdotto;
+        public string nomeProdotto;
+        public string descrizioneProdotto;
+        public float prezzoNoIVA;
+        public float percentualeIVA;
+        public float prezzoIvato;
+        public Prodotto(string nomeProdotto, string descrizioneProdotto, float prezzoNoIVA, float percentualeIVA)
+        {
+            Random rnd = new Random();
+            int codiceProdotto = rnd.Next();
+            this.codiceProdotto = codiceProdotto;
+
+            this.nomeProdotto = nomeProdotto;
+            this.descrizioneProdotto = descrizioneProdotto;
+            this.prezzoNoIVA = prezzoNoIVA;
+            this.percentualeIVA = percentualeIVA;
+        }
+
+
+        public string calcPrezzoIvato()
+        {
+            float prezzoIvato = (prezzoNoIVA/100) * percentualeIVA + prezzoNoIVA;
+
+            return prezzoIvato.ToString("N2");
+
+
+        }
+
+
+        public int GetCodiceProdotto()
+        {
+
+            return codiceProdotto;
+
+        }
+
+
+       
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using csharp_oop_shop;
+
+
+
+Prodotto penna = new Prodotto ("Penna Bic","Una penna senza pretese.", 1.5f, 22f);
+
+
+Console.WriteLine(penna.nomeProdotto);
+Console.WriteLine($"CODICE PRODOTTO: {penna.GetCodiceProdotto()}");
+Console.WriteLine(penna.descrizioneProdotto);
+Console.WriteLine($"Prezzo senza IVA: {penna.prezzoNoIVA} EUR");
+Console.WriteLine($"Prezzo IVA inclusa con IVA del {penna.percentualeIVA}% : {penna.calcPrezzoIvato()} EUR");
+Console.WriteLine("-----------------------------------------------------");
+
+
+Prodotto saldatore = new Prodotto("Saldatore Hakko","Un'eccellente stazione saldante per gli appassionati del fai da te!", 149.99f, 22f);
+
+Console.WriteLine(saldatore.nomeProdotto);
+Console.WriteLine($"CODICE PRODOTTO: {saldatore.GetCodiceProdotto()}");
+Console.WriteLine(saldatore.descrizioneProdotto);
+Console.WriteLine($"Prezzo senza IVA: {saldatore.prezzoNoIVA} EUR");
+Console.WriteLine($"Prezzo IVA inclusa con IVA del {saldatore.percentualeIVA}% : {saldatore.calcPrezzoIvato()} EUR");
+Console.WriteLine("-----------------------------------------------------");
